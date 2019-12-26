1f88c8134dfb481f73e08b0fcbb19441f51e0dd4
waspryo <wasp2123321@gmail.com>
2019-12-26 23:45:37 +0900

    done routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :records
-  resources :artists
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :records
+      resources :artists
+      # api/v1/records
+      # api/v1/artists
+    end
+  end
+
+  root to: "home#index"
+
 end
